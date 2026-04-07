9fb22dff494ca87b3f165fd936d1cec5a56cbb0b
JOSHUACURIE <joshuajuma039@gmail.com>
2026-04-08 00:06:33 +0300

    added the message response dto

--- src/main/java/com/Lchat/Lchat/dto/MessageResponseDTO.java
+++ src/main/java/com/Lchat/Lchat/dto/MessageResponseDTO.java
@@ -27,8 +27,37 @@ public class MessageResponseDTO {
    this.content=content;
    this.timestamp=timestamp;
    this.isRead=isRead;
-    his.type=type;
+    this.type=type;
     }
+
+    public Long getId(){ return id;}
+    public void setId( Long id){ this.id=id;}
+
+    public Long getSenderId(){ return senderId;}
+    public void setSenderId(Long senderId){ this.senderId=senderId;}
+
+    public String getSenderName(){ return senderName;}
+    public void setSenderName(String senderName){ this.senderName=senderName;}
+
+    public Long getReceiverId(){ return receiverId;}
+    public void setReceiverId(Long receiverId){ this.receiverId=receiverId;}
+
+    public String getReceiverName(){ return receiverName;}
+    public void setReceiverName(String receiverName){ this.receiverName=receiverName;}
+
+    public String getContent(){ return content;}
+    public void setContent(String content){ this.content=content;}
+
+    public LocalDateTime getTimeStamp(){ return timestamp;}
+    public void setTimeStamp(LocalDateTime timestamp){ this.timestamp=timestamp;}
+
+    public boolean getIsRead(){ return isRead;}
+    public void setIsRead(boolean isRead){ this.isRead=isRead;}
+
+    public String getType(){ return type;}
+    public void setType(String type){ this.type=type;}
+
+
 
 
 }
