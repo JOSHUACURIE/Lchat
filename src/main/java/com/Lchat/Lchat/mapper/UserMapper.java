2210e37558f92994fca99680d14e7061dcceff19
JOSHUACURIE <joshuajuma039@gmail.com>
2026-04-07 09:09:30 +0300

    finished working on the user mapper

--- src/main/java/com/Lchat/Lchat/mapper/UserMapper.java
+++ src/main/java/com/Lchat/Lchat/mapper/UserMapper.java
@@ -9,5 +9,48 @@ import java.time.LocalDateTime;
 public class UserMapper {
     
 //convert registration dto into entity for user creation
+public User toEntity(UserRegistrationDTO dto){
+    if(dto==null){
+        return null;
+    }
+    User user=new User();
+    user.setUsername(dto.getUsername());
+    user.setName(dto.getName());
+    user.setEmail(dto.getEmail());
+    user.setPassword(dto.getPassword());
+    user.setChatStatus(ChatStatus.OFFLINE);
+    user.setLastActive(LocalDateTime.now());
+
+return user;
+}
     
+//convert entity into dto
+public UserResponseDTO toResponseDTO(User user){
+    if(user==null){ return null;}
+
+    UserResponseDTO dto=new UserResponseDTO();
+    dto.setId(user.getId());
+    dto.setUsername(user.getUsername());
+    dto.setName(user.getName());
+    dto.setEmail(user.getEmail());
+    dto.setChatStatus(user.geChatStatus());
+    dto.setLastActive(user.getLastActive());
+    dto.setCreatedAt(user.getCreatedAt());
+
+return dto;
+}
+//update entity from dto
+public void updateEntity(UserUpdateDTO dto,User user ){
+    if( dto == null || user ==null){ return; }
+
+    if(dto.getUsername() != null){ user.setUsername(dto.getUsername());}
+
+    if(dto.getName() != null){ user.setName(dto.getName());}
+
+    if(dto.getEmail() != null){ user.setEmail(dto.getEmail());}
+
+    if(dto.getPassword() != null){ user.setPassword(dto.getPassword());}
+
+}
+
 }
